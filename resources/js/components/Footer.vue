91949d2bffbb6d090fc2290757da43d755028522
soysource8850 <lucid.sky.snl@gmail.com>
2019-06-06 10:17:14 +0900

    認証状態によるナビゲーションバーとフッターの要素を出し分ける処理追加

--- resources/js/components/Footer.vue
+++ resources/js/components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
   <footer class="footer">
-    <button class="button button--link" @click="logout">Logout</button>
-    <RouterLink class="button button--link" to="/login">Login / Register</RouterLink>
+    <button v-if="isLogin" class="button button--link" @click="logout">Logout</button>
+    <RouterLink v-else class="button button--link" to="/login">Login / Register</RouterLink>
   </footer>
 </template>
 
@@ -11,6 +11,11 @@ export default {
     async logout() {
       await this.$store.dispatch("auth/logout");
       this.$router.push("/login");
+    }
+  },
+  computed: {
+    isLogin() {
+      return this.$store.getters["auth/check"];
     }
   }
 };
