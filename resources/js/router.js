bf6de82bebc1b86935998ed351a67e4d7df2e2cc
soysource8850 <lucid.sky.snl@gmail.com>
2019-06-06 13:10:36 +0900

    ログインページに対してナビゲーションガードを実装

--- resources/js/router.js
+++ resources/js/router.js
@@ -1,31 +1,33 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
-// ページコンポーネントをインポートする
 import PhotoList from './pages/PhotoList.vue'
 import Login from './pages/Login.vue'
 
-// VueRouterプラグインを使用する
-// これによって<RouterView />コンポーネントなどを使うことができる
+import store from './store'
+
 Vue.use(VueRouter)
 
-// パスとコンポーネントのマッピング
 const routes = [{
     path: '/',
     component: PhotoList
   },
   {
     path: '/login',
-    component: Login
+    component: Login,
+    beforeEnter: (to, from, next) => {
+      if (store.getters['auth/check']) {
+        next('/')
+      } else {
+        next()
+      }
+    }
   }
 ]
 
-// VueRouterインスタンスを作成する
 const router = new VueRouter({
   mode: 'history',
   routes
 })
 
-// VueRouterインスタンスをエクスポートする
-// app.jsでインポートするため
 export default router
