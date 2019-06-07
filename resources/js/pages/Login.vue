d637e8ce9c20701dd2cfa30e7b1604db29dce724
soysource8850 <lucid.sky.snl@gmail.com>
2019-06-07 11:24:32 +0900

    コンポーネントにエラーページ遷移処理

--- resources/js/pages/Login.vue
+++ resources/js/pages/Login.vue
@@ -55,10 +55,17 @@ export default {
       }
     };
   },
+  computed: {
+    apiStatus() {
+      return this.$store.state.auth.apiStatus;
+    }
+  },
   methods: {
     async login() {
       await this.$store.dispatch("auth/login", this.loginForm);
-      this.$router.push("/");
+      if (this.apiStatus) {
+        this.$router.push("/");
+      }
     },
     async register() {
       await this.$store.dispatch("auth/register", this.registerForm);
