2b0163c355e4f08beba8be6297a3650f8b78495c
soysource8850 <lucid.sky.snl@gmail.com>
2019-06-07 10:47:19 +0900

    authストアに例外処理追加

--- resources/js/store/auth.js
+++ resources/js/store/auth.js
@@ -1,5 +1,10 @@
+import {
+  OK
+} from '../util'
+
 const state = {
-  user: null
+  user: null,
+  apiStatus: null
 }
 
 const getters = {
@@ -14,18 +19,33 @@ const mutations = {
 }
 
 const actions = {
+
   async register(context, data) {
     const response = await axios.post('/api/register', data)
     context.commit('setUser', response.data)
   },
+
   async login(context, data) {
-    const response = await axios.post('/api/login', data)
-    context.commit('setUser', response.data)
+    context.commit('setApiStatus', null)
+    const response = await axios.post('/api/login', data).catch(err => err.response || err)
+
+    if (response.status === OK) {
+      context.commit('setApiStatus', true)
+      context.commit('setUser', response.data)
+      return false
+    }
+
+    context.commit('setApiStatus', false)
+    context.commit('error/setCode', response.status, {
+      root: true
+    })
   },
+
   async logout(context) {
     const response = await axios.post('/api/logout')
     context.commit('setUser', null)
   },
+
   async currentUser(context) {
     const response = await axios.get('/api/user')
     const user = response.data || null
